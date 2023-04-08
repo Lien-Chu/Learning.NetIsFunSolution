f4896657d340d95aa8910914d87939d4963f6186
LIEN-CHU HUANG <vickykilumanga@gmail.com>
2023-04-08 16:47:42 +0200

    add productDbContext

---
+++ Learning.NetIsFun/Data/ProductDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Learning.NetIsFun.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Learning.NetIsFun.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductModel> Product { get; set; }
+    }
+}
